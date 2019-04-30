0fd491898198a5ea87b544962b0247a47a7fd1a6
HieuNa <nguyenanhhieu231298@gmail.com>
2019-04-30 21:46:56 +0700

    Update Chức năng Message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {EmojiConvertor} from 'emoji-js';
+import ReactDOM from 'react-dom';
 
-const Message = (props) => {  
+const Message = (props) => { 
+
+    const emoji = new EmojiConvertor();
+
+    emoji.img_sets.google.path = 'https://res.cloudinary.com/dswwcaqwu/image/upload/v1555303638/emoji-data/img-google-64';
+    emoji.img_sets.google.sheet = 'https://res.cloudinary.com/dswwcaqwu/image/upload/v1555303638/emoji-data/sheet_google_64.png';
+
+    emoji.use_sheet = true;
+
+    emoji.img_set = 'google';
+
     return (
         <div className="message">
             <div className="message-username">{props.username}</div>
-            <div className="message-text">{props.text}</div>
+            <div className="message-text" dangerouslySetInnerHTML={{__html: emoji.replace_emoticons(props.text)}}></div>
         </div>
     )
 }
